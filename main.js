50fd6d9b109323773a7eeb3354463809fc719712
MrChest <zookeeper.uob@outlook.com>
2026-09-05 21:38:22 +0800

    a nerdy style changed by codex and two more blogs

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
-fetch("/nav.html")
-    .then(response=>response.text())
-    .then(
-        html=>{
-            document
-                .querySelector("#nav-rep")
-                .insertAdjacentHTML("beforeend",html)
+const navTarget = document.querySelector("#nav-rep");
+const scriptUrl = document.currentScript?.src ?? window.location.href;
+const navUrl = new URL("nav.html", scriptUrl);
+const navFallback = `
+    <nav id="nav">
+        <ul id="nu">
+            <li><a href="/index.html">主页</a></li>
+            <li><a href="/blog.html">博客</a></li>
+            <li><a href="/works.html">项目</a></li>
+        </ul>
+    </nav>
+`;
+
+function mountNavigation(html) {
+    if (!navTarget) return;
+
+    navTarget.insertAdjacentHTML("beforeend", html);
+
+    const siteBase = new URL("./", scriptUrl);
+    navTarget.querySelectorAll('a[href^="/"]').forEach(link => {
+        link.href = new URL(link.getAttribute("href").slice(1), siteBase);
+    });
+}
+
+fetch(navUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Navigation request failed: ${response.status}`);
         }
-    )
+
+        return response.text();
+    })
+    .then(html => {
+        mountNavigation(html);
+    })
+    .catch(error => {
+        console.error(error);
+        mountNavigation(navFallback);
+    });
